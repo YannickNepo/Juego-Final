d64dddf56e3ec3db497a300fc96eb8fd349730c4
YannickNepo <101335474+YannickNepo@users.noreply.github.com>
2022-06-03 11:57:48 -0300

    Agregue cartel de perder

--- Assets/playercollision.cs
+++ Assets/playercollision.cs
@@ -1,38 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playercollision : MonoBehaviour
 {
     public Vector3 originalPosition;
+    public GameObject perdiste;
+    float tiempo;
 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.transform.position = originalPosition;
-        originalPosition = new Vector3(0, 2, 0);
+        originalPosition = new Vector3(0, 1, 0);
+        perdiste.SetActive(!perdiste.activeInHierarchy);
     }
 
     // Update is called once per frame
     void Update()
     {
- 
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.name == "DeathWall")
+        if (Time.time > tiempo+3)
         {
-            gameObject.transform.position = originalPosition;
+            perdiste.SetActive(false);
         }
     }
+
+    //void OnCollisionEnter(Collision col)
+    //{
+    //    if (col.gameObject.name == "DeathWall")
+    //    {
+    //        gameObject.transform.position = originalPosition;
+    //    }
+    //}
 
     void OnCollisionExit(Collision col2)
     {
-        if (col2.gameObject.name == "Ground")
+        if (col2.gameObject.name == "Piso")  
         {
-            transform.position = originalPosition;
+            perdiste.SetActive(true);
+            gameObject.transform.position = originalPosition;
+            tiempo = Time.time;
         }
+        
+        
     }
 
 }
